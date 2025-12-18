c6610bb270fbec0d869b34c26703eff77c486ff5
khoi-thinh <thinhduckhoi@gmail.com>
2025-12-18 09:02:10 +0900

    update files

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "cloudcore007"
+    bucket = "devsecops-terraform-khoi"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -44,19 +44,19 @@ variable "iam-role" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0c7217cdde317cfec" // Replace with the latest AMI ID for your region
+  default     = "ami-068c0051b15cdb816"
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.large"
+  default     = "t3.micro"
 }
 
 variable "key_name" {
   description = "EC2 keypair"
   type        = string
-  default     = "gitopskey"
+  default     = "devops"
 }
 
 variable "instance_name" {
